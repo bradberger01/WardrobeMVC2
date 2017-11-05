8ba7d727b7a12880da96b5fb209d73f75a16e66f
bradberger01 <bradberger@hotmail.com>
2017-11-05 13:25:36 -0500

    first commit

---
+++ WardrobeMVC2/Models/WardrobeMVC2Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WardrobeMVC2.Models
+{
+    public class WardrobeMVC2Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WardrobeMVC2Context() : base("name=WardrobeMVC2Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WardrobeMVC2.Models.Accessory> Accessories { get; set; }
+
+        public System.Data.Entity.DbSet<WardrobeMVC2.Models.Bottom> Bottoms { get; set; }
+
+        public System.Data.Entity.DbSet<WardrobeMVC2.Models.Shoe> Shoes { get; set; }
+
+        public System.Data.Entity.DbSet<WardrobeMVC2.Models.Top> Tops { get; set; }
+    }
+}
